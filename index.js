782430d46ebcf679c000ae99ee9ec9a5193d8a2d
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-10-28 22:25:07 +0530

    added keys from env

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-import AWSIoTData from "aws-iot-device-sdk";
 import AWS from "aws-sdk";
 import fs from "fs";
 import { getPrinters, print } from "unix-print";
+import { AWSCredentials, AWSIoTClient } from "./config/aws";
 
 getPrinters().then(console.log);
 
-const AWSCredentials = {
-  accessKeyId: "ASIAULIGBHUUZLJWBQNF",
-  secretAccessKey: "zbO1KteyqkP/rqducUG2Ve9V2VkSK23cDDW1vPCA",
-  sessionToken:
-    "FwoGZXIvYXdzEIv//////////wEaDHnEjKe7a9CmUiz0HyK3ASVp6AUV/S69GgRYj+yOadN0xGRygi73XIkN37qTB33mYxSMWaHCVIvISOSdniyRsG11cwxBKZ4yHLZilntPGoa9y3G2Jw3w+HrGix3pv4vUAP0ZoH+0xbw048c6sJ7G3yVwlC8ViPf+Wdgr8y6qQkG3eAwPkD0GXgn5Rs8HTs6n7u7tKGS/86ooT+pR+NXiQ31OHapwa3YchVI6Gun+FPqwWW2RAAhJi5EHbBcFxqrxm4I6moxxaCikqfOpBjIto64CxmdBshRo9q7ZfYItxsLTXmcDETYS90NXsQseJYEm6Sdd04lP2T8nZc1Z",
-};
 const s3 = new AWS.S3({ credentials: AWSCredentials, region: "us-east-1" });
 
-const device = AWSIoTData.device({
-  keyPath: "./keys/private.key",
-  caPath: "./keys/rootCa.pem",
-  certPath: "./keys/certificate.crt",
-  clientId: "raspberry-pi-device",
-  host: "a2uaokcdvkjoer-ats.iot.us-east-1.amazonaws.com",
-  cleanSession: false,
-});
+const device = AWSIoTClient("raspberry-pi-device");
 
 device.on("connect", () => {
   console.log("connected");
